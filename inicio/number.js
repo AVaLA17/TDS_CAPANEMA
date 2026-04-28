913e149c54e1a0e0bf3d0330bf01d8c5a1c6df33
Avila <xinlofi889@gmail.com>
2026-04-27 21:47:58 -0300

    Fundamentos de JavaScript

---
+++ inicio/number.js
@@ -0,0 +1,11 @@
+let idade = 24;
+let altura = 1.77;
+
+console.log(idade);
+console.log(altura);
+
+
+console.log(idade + altura); //Somar
+console.log(idade, altura); //Concatenar
+
+
---
+++ inicio/templateString.js
@@ -0,0 +1,8 @@
+let nome = "Gabriel";
+let frase  = "Olá" + nome + ", \nseja bem-vindo!";
+console.log(frase);
+
+//template String
+frase = `Olá ${nome},
+seja bem-vindo!`;
+console.log(frase);
